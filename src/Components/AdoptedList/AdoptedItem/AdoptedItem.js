00bf7448a53756597299b50bd998f4a677724308
Leon Dail <leondailofficial@gmail.com>
2019-11-15 15:02:47 -0800

    assets

--- src/Components/AdoptedList/AdoptedItem/AdoptedItem.js
+++ src/Components/AdoptedList/AdoptedItem/AdoptedItem.js
@@ -1,15 +1,11 @@
 //Dependencies
 import React from 'react';
 
-function checkSprite(type) {
-
-}
-
 function AdoptedItem(props) {
   return(
     <li className="adoptedItem">
       <div className="animalSprite">
-        <img src={props.petType === 'dog' ? '../../../assets/dogsprite.png' : '/assets/catsprite.png' } alt={props.petType} />
+        <img src={props.petType === 'dog' ? '/assets/dogsprite.png' : '/assets/catsprite.png' } alt={props.petType} />
       </div>
       <div className="backgroundRow">
         <div className="nameInfo">
